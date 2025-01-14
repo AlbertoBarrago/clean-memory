dbc21fdd2486ac1032538cbe9ddb4a3d76c7654f
Alberto Barrago <albertobarrago@gmail.com>
2025-01-15 00:22:27 +0100

    Update

--- clean-memory.rb
+++ clean-memory.rb
@@ -1,7 +1,7 @@
 class CleanMemory < Formula
   desc "This script contains a collection of commands that are used to update system resources and software"
   homepage "https://github.com/AlbertoBarrago/clean_memory"
-  url "https://github.com/AlbertoBarrago/clean_memory/archive/v1.0.0.tar.gz"
+  url "https://github.com/AlbertoBarrago/clean_memory/archive/refs/tags/1.0.0.tar.gz"
   sha256 "7fdfc166b7cb35614bc0d61d25153331c7a13af730e15012c75b8c9d2906fa86"
   version "1.0.0"
 
