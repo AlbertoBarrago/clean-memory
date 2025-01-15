c2031db86788cc0406f8ae0fef6fbb8023740d52
alBz <albertobarrago@gmail.com>
2025-01-15 10:22:43 +0100

    Update clean-memory.rb

--- clean-memory.rb
+++ clean-memory.rb
@@ -1,7 +1,7 @@
 class CleanMemory < Formula
   desc "This script contains a collection of commands that are used to update system resources and software"
-  homepage "https://github.com/AlbertoBarrago/clean_memory"
-  url "https://github.com/AlbertoBarrago/clean_memory/archive/refs/tags/1.0.0.tar.gz"
+  homepage "https://github.com/AlbertoBarrago/cleanMemory"
+  url "https://github.com/AlbertoBarrago/cleanMemory/archive/refs/tags/1.0.0.tar.gz"
   sha256 "7fdfc166b7cb35614bc0d61d25153331c7a13af730e15012c75b8c9d2906fa86"
   version "1.0.0"
 
@@ -14,7 +14,6 @@ class CleanMemory < Formula
       This script requires sudo privileges to execute some commands.
       Use it as follows:
       - To run the script: clean-memory
-      - Additional commands and options can be found in the documentation.
     EOS
   end
 end
